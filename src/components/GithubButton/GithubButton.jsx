6811b99521bdd85c62e916e62950686635caf630
rusanov-dmitry <65367402+rusanov-dmitry@users.noreply.github.com>
2021-09-06 17:55:30 +0600

    Edit link attributes

--- src/components/GithubButton/GithubButton.jsx
+++ src/components/GithubButton/GithubButton.jsx
@@ -4,7 +4,7 @@ import githubIcon from '../../assets/github-icon.svg';
 
 const GithubButton = () => {
     return (
-        <a href="https://github.com/rusanov-dmitry" className={styles.button} target="_blank">
+        <a href="https://github.com/rusanov-dmitry" className={styles.button} target="_blank" rel="noreferrer">
             <img src={githubIcon} alt="Github Icon"/>
             VISIT MY GITHUB
         </a>
--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -9,22 +9,22 @@ const MyProjects = () => {
         <section className={styles.container}>
             <h1 className={styles.title}>My projects</h1>
             <div className={styles.projectInfo}>
-                <img src={testselfPreview} alt="Preview image"/>
+                <img src={testselfPreview} alt="Preview"/>
                 <div className={styles.projectDescription}>
                     <h2>TestSelf</h2>
                     <p>Quiz app built with React + Redux.
                         You can check it here:
-                        <a href="https://testself.netlify.app/" target="_blank">testself.netlify.app</a></p>
+                        <a href="https://testself.netlify.app/" target="_blank" rel="noreferrer">testself.netlify.app</a></p>
                 </div>
             </div>
                 <hr/>
             <div className={styles.projectInfo}>
-                <img src={splittipsPreview} alt="Preview image"/>
+                <img src={splittipsPreview} alt="Preview"/>
                 <div className={styles.projectDescription}>
                     <h2>Split Tips</h2>
                     <p>Tip calculator app built with React using React Hooks.
                         Check it out:
-                        <a href="https://splittips.netlify.app/" target="_blank">splittips.netlify.app</a></p>
+                        <a href="https://splittips.netlify.app/" target="_blank" rel="noreferrer">splittips.netlify.app</a></p>
                 </div>
             </div>
             <GithubButton />
